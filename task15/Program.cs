718d4eb250bd61f5a138cb86646256f32d9bc0a3
Dmitry <spitsynfilmeffect@gmail.com>
2022-09-07 20:01:25 +0300

    ïts all over

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Добро пожаловать в программу определения выходных");
+Console.WriteLine("Введите  число, обозначающее номер дня недели");
+Int32 day = Convert.ToInt32(Console.ReadLine());
+if (day <=5){
+    System.Console.WriteLine("Это будний день");
+}
+else
+System.Console.WriteLine("Это выходной");
+
